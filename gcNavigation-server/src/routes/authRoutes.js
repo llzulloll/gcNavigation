fcc80d073921b77a382ee2ed9ae8e1f14e14a231
Lorenzo Zullo <llzulloll@gmail.com>
2024-07-08 20:38:46 -0400

    ngrok server functionality

--- gcNavigation-server/src/routes/authRoutes.js
+++ gcNavigation-server/src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const User = mongoose.model('User');
 
-const router = express.Router();
+const router = express.Router()
 
 router.post('/signup', async (req, res) => {
 
